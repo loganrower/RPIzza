3e9f414bd6705d051ed74df859681ef70e5531b9
Sebastian Hardin <60301051+hardins01@users.noreply.github.com>
2023-08-21 19:25:04 -0700

    LCD Functions (#5)
    I wrote this code (@hardins01), but the commits are made by @loganrower
    because I made the commits on his Raspberry Pi.
    
    I created an LCD class to make it very simple to interact with the
    display. The class also manages the logic of automatic scrolling of
    messages if they're too long for the width of the display. I added
    documentation for the LCD class in `hardware/lcd/lcd.md`.
    
    ---------
    
    Co-authored-by: loganrower <lkrower2000@gmail.com>

--- hardware/hardware.py
+++ hardware/hardware.py
@@ -1,8 +1,8 @@
-# packages for LCD
-from lcd_poc.PCF8574 import PCF8574_GPIO
-from lcd_poc.Adafruit_LCD1602 import Adafruit_CharLCD
 from time import sleep, strftime
 from datetime import datetime
+
+from lcd.lcd import LCD
+
 
 # packages for Keypad
 import RPi.GPIO as GPIO
@@ -20,14 +20,7 @@ rowsPins = [12,16,18,22]        #connect to the row pinouts of the keypad
 colsPins = [19,15,13,11]        #connect to the column pinouts of the keypad
 
 
-# LCD I2C
-PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
-PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
-# Create PCF8574 GPIO adapter.
-
 def loop():
-    mcp.output(3,1)     # turn on LCD backlight
-    lcd.begin(16,2)     # set number of LCD lines and columns
     keypad = Keypad(keys,rowsPins,colsPins,ROWS,COLS)    #creat Keypad object
     keypad.setDebounceTime(50)      #set the debounce time
     print("Press key to start")
@@ -35,32 +28,20 @@ def loop():
         key = keypad.getKey()       #obtain the state of keys
         if(key != keypad.NULL):     #if there is key pressed, print its key code.
             print ("You Pressed Key : %c "%(key))
-            #lcd.clear()
-            lcd.setCursor(0,0)  # set cursor position
-            lcd.message('Order Number:\n')# display CPU temperature
-            lcd.message(key)
+            lcd.write("this is a longer message that should scroll", row=0)
+            lcd.write(f"{key} is the button that you pressed", row=1)
             sleep(1)
     # Full order number
     ## order_number = ""
     ## while star is not pressed then continue to add to the order number
-    ## 
+    ##
     ## lcd.message
-        
+
 def destroy():
     lcd.clear()
     GPIO.cleanup()
-    
 
-try:
-    mcp = PCF8574_GPIO(PCF8574_address)
-except:
-    try:
-        mcp = PCF8574_GPIO(PCF8574A_address)
-    except:
-        print ('I2C Address Error !')
-        exit(1)
-# Create LCD, passing in MCP GPIO adapter.
-lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+lcd = LCD()
 
 if __name__ == '__main__':
     print ('Program is starting ... ')
@@ -68,3 +49,4 @@ if __name__ == '__main__':
         loop()
     except KeyboardInterrupt:
         destroy()
+
---
+++ hardware/lcd/lcd.py
@@ -0,0 +1,96 @@
+from threading import Thread, Lock
+import time
+
+from .PCF8574 import PCF8574_GPIO
+from .Adafruit_LCD1602 import Adafruit_CharLCD
+
+
+PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+
+LCD_WIDTH = 16   # number of characters per horizontal row
+LCD_HEIGHT = 2   # number of rows
+
+
+class LCD:
+    def __init__(self):
+        try:
+            self.mcp = PCF8574_GPIO(PCF8574_address)
+            self.lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=self.mcp)
+            self.lcd.begin(LCD_WIDTH, LCD_HEIGHT)
+            self.lcd_lock = Lock()
+            self.scroll_threads = []
+            self.scroll_threads_terminate = []
+            for thread in range(0, LCD_HEIGHT):
+                self.scroll_threads.append(None)
+                self.scroll_threads_terminate.append(False)
+        except:
+            try:
+                self.mcp = PCF8574_GPIO(PCF8574A_address)
+            except:
+                raise Exception("LCD not found at expected I2C addresses")
+
+
+    def clear(self):
+        for thread_index in range(0, len(self.scroll_threads)):
+            if self.scroll_threads[thread_index] is not None and self.scroll_threads[thread_index].is_alive():
+                self.scroll_threads_terminate[thread_index] = True
+                self.scroll_threads[thread_index].join()
+        self.lcd_lock.acquire()
+        self.lcd.clear()
+        self.lcd_lock.release()
+
+
+    # row is 0-indexed, default is 0
+    def write(self, message, row=0):
+        self.mcp.output(3, 1)
+
+        # kill a scrolling thread if there's already one on this row
+        if self.scroll_threads[row] is not None and self.scroll_threads[row].is_alive():
+            self.scroll_threads_terminate[row] = True
+            self.scroll_threads[row].join()
+
+        if len(message) > LCD_WIDTH:
+            self.scroll_threads_terminate[row] = False
+            self.scroll_threads[row] = Thread(target=scroll, args=[self.lcd, self.lcd_lock, LCD_WIDTH, message, row, lambda:self.scroll_threads_terminate[row]])
+            self.scroll_threads[row].start()
+            return
+
+        # calculate cursor to place message in center
+        difference = LCD_WIDTH - len(message)
+        cursor_pos = difference // 2
+
+        self.lcd_lock.acquire()
+        self.lcd.setCursor(cursor_pos, row)
+        self.lcd.message(message)
+        self.lcd_lock.release()
+
+
+def scroll(lcd, lcd_lock, lcd_width, message, row, stop):
+    start_pause = 2     # seconds before scrolling starts
+    end_pause = 2       # seconds after scrolling starts before looping back to the beginning
+    scroll_speed = 0    # seconds per character scroll
+    while True:
+        window_start = 0
+        window_end = lcd_width
+
+        lcd_lock.acquire()
+        lcd.setCursor(0, row)
+        lcd.message(message[window_start:window_end])
+        lcd_lock.release()
+
+        time.sleep(start_pause)
+        while window_end < len(message):
+            if stop():
+                return
+
+            window_start += 1
+            window_end += 1
+
+            lcd_lock.acquire()
+            lcd.setCursor(0, row)
+            lcd.message(message[window_start:window_end])
+            lcd_lock.release()
+
+            time.sleep(scroll_speed)
+        time.sleep(end_pause)
